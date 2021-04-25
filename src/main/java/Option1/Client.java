7738d9216bf77b2b754fbc98025d585db9091244
Sebin <sebinsuresh@gmail.com>
2021-04-24 23:19:45 -0500

    Changes by OBS - message arrived on Client stuff

--- src/main/java/Option1/Client.java
+++ src/main/java/Option1/Client.java
@@ -19,13 +19,19 @@
 
 package Option1;
 
+import com.google.gson.Gson;
 import org.eclipse.paho.client.mqttv3.*;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class Client {
 
     // MQTT Client ID for the class
     final static String CLIENT_ID = "Team2_ClientMachine";
+    private Gson gson;
+
 
     private MqttClient mqttClient;
 
@@ -48,6 +54,7 @@ public class Client {
         this.mqttClient = new MqttClient(
                 App.MQTT_BROKER, CLIENT_ID, new MemoryPersistence()
         );
+
         MqttConnectOptions connOpts = new MqttConnectOptions();
         connOpts.setCleanSession(true);
 
@@ -62,8 +69,29 @@ public class Client {
         // Set the callback for the client
         mqttClient.setCallback(new MqttCallback() {
             @Override
-            public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
-                System.out.println("Message has arrived on Client Machine via MQTT.");
+            public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+                System.out.println("Message has arrived on Client Machine via MQTT." + "\n" + topic +
+                        "\n\tMessage: " + new String(mqttMessage.getPayload()));
+
+                // TODO - wait for 2 peer machine replies before new search
+
+                // The user, through the terminal/GUI, can type one of the commands
+                // prompt user for another command?
+                System.out.println("Enter a first name: ");
+                String statement = "";
+                Scanner in = new Scanner(System.in);
+                statement = in.next();
+
+                // TODO - edit row
+                String [] row = {statement, "", "", "", ""};
+                Message msg = new Message(App.type.CL_2_PM, null, null, null);
+                gson = new Gson();
+                String toSend = gson.toJson(msg);
+
+                // publish to both peer machines
+                mqttMessage = new MqttMessage(toSend.getBytes());
+                mqttMessage.setQos(1);
+                mqttClient.publish("/CS341FinalProj/Team2/FromClient", mqttMessage);
             }
 
             @Override
