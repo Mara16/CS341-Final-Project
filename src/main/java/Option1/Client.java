a592bfbde3dd32457184055ac4f3f70299dd1a16
Sebin <sebinsuresh@gmail.com>
2021-04-25 22:48:22 -0500

    Cleaning up, avoiding all-blank-values, formatting, etc

--- src/main/java/Option1/Client.java
+++ src/main/java/Option1/Client.java
@@ -66,39 +66,39 @@ public class Client {
 
         // Subscribing to topic - QoS 1
         mqttClient.subscribe("/CS341FinalProj/Team2/FromPeerMachine/#", 1);
-        System.out.println("Subscribed");
+        // System.out.println("Subscribed");
 
         // Set the callback for the client
         mqttClient.setCallback(new MqttCallback() {
             @Override
             public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
-                System.out.println("\nA message has arrived on Client Machine via MQTT" + "\nfrom topic: " + topic +
-                        "\nMessage: " + new String(mqttMessage.getPayload()) + "\n");
 
-                // TODO - display message formatted, currently received as
-                //  {"type":"PM_2_CL","results":[["Harry","Potter","05180 Sparks Run Port Keith. MO 48921","58000","32","PM1_W3"] ,["Fleamont","Potter","575 Fuller Rapids Suite 993 East Michael. TX 38202","40000","26","PM1_W1"]]}
                 String msgJson = new String(mqttMessage.getPayload());
                 Message msgFromPM = gson.fromJson(msgJson, Message.class);
 
                 // determine which peer machine is replying
                 String repliedMachineName = msgFromPM.msg;
-                System.out.println(repliedMachineName + " reporting results: ");
+                System.out.println("\n" + repliedMachineName + " reporting results: ");
 
                 // output result to user
                 List<String[]> result = msgFromPM.results;
 
                 result.forEach(strings -> {
-                    System.out.print("Result: ");
+                    System.out.print("| ");
                     for (int i = 0; i < strings.length; i++) {
-                        System.out.print(strings[i] + " ");
+                        System.out.print(strings[i] + " | ");
 
                     }
-                    System.out.println("\n");
+                    System.out.println();
                 });
+                if(result.size() == 0)
+                    System.out.println("No results found.");
+                System.out.println(nonFancyDivider);
 
                 numberOfResponses++;
                 // The user, through the terminal/GUI, can send another query if both PeerMachines have replied
                 if (numberOfResponses == 2) {
+                    System.out.println("\n" + fancyDivider);
                     userInput();
                 }
             }
@@ -110,7 +110,7 @@ public class Client {
 
             @Override
             public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
-                System.out.println("Client machine delivered message to Broker.");
+                // System.out.println("Client machine delivered message to Broker.");
             }
         });
     }
@@ -127,20 +127,33 @@ public class Client {
         System.out.println("\nEnter a first name: ");
         firstName = in.nextLine();
 
-        System.out.println("\nEnter a last name: ");
+        System.out.print("Enter a last name: ");
         lastName = in.nextLine();
 
-        System.out.println("\nEnter an address name: ");
+        System.out.print("Enter an address name: ");
         address = in.nextLine();
 
-        System.out.println("\nEnter a salary: ");
+        System.out.print("Enter a salary: ");
         salary = in.nextLine();
 
-        System.out.println("\nEnter their age: ");
+        System.out.print("Enter their age: ");
         age = in.nextLine();
 
         // TODO - edit row
         String[] row = {firstName, lastName, address, salary, age};
+
+        boolean atLeastone = false;
+        for(String queryField: row){
+            if(queryField != null & !queryField.trim().equals("")){
+                atLeastone = true;
+            }
+        }
+        if(!atLeastone){
+            System.err.println("Please enter at least one value!!!");
+            userInput();
+            return;
+        }
+
         Message msg = new Message(App.type.CL_2_PM, null, row, null);
         gson = new Gson();
         String toSend = gson.toJson(msg);
@@ -150,5 +163,9 @@ public class Client {
         mqttMessage.setQos(1);
         mqttClient.publish("/CS341FinalProj/Team2/FromClient", mqttMessage);
         numberOfResponses = 0;
+
+        System.out.println();
+        System.out.print(fancyDivider);
+        System.out.println();
     }
 }
